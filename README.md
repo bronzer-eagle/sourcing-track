aee79f842e6cc57ea5fe0099e412b0b71e91e16a
alexanderbondarenko <alexander.bondarenko@itrexgroup.com>
2017-09-10 18:33:49 +0300

    Testing deploy through github

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A little web app for Linkedin sourcers
 
 "Procfile" - is heroku platform file, for starting app.
+
+## Heroku commands
+`heroku logs --tail` - show logs
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World, we are deploying with github!');
 });
 
 app.listen(process.env.PORT || 5000, function () {
